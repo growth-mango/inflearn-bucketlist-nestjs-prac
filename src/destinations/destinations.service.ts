a9733e6250b4acbd02c80846d556f9b9984e4de8
growth-mango <esudatcom@gmail.com>
2025-08-05 17:59:33 +0900

    feat: add destination description search

--- src/destinations/destinations.service.ts
+++ src/destinations/destinations.service.ts
@@ -28,9 +28,14 @@ export class DestinationsService {
 
   async search(q: string): Promise<Destination[]> {
     return this.destinationsRepository.find({
-      where: {
-        name: Like(`%${q}%`),
-      },
+      where: [
+        {
+          name: Like(`%${q}%`),
+        },
+        {
+          description: Like(`%${q}%`),
+        },
+      ],
     });
   }
 
